820fbacb944afe681e2124d3e13834024a92d96f
Vladimir_Kostik <vladimirkostik48@gmail.com>
2026-05-02 21:36:03 +0300

    complete express server

---
+++ src/server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import 'dotenv/config';
+import pino from 'pino-http';
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(pino());
+
+
+
+// GET /students/:studentId
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+// GET /notes/:noteId
+app.get('/notes/:noteId', (req, res) => {
+  res.status(200).json({
+    message: `Retrieved note with ID: ${req.params.noteId}`,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// 404
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// error middleware
+app.use((err, req, res) => {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd ? 'Щось пішло не так :)' : err.stack,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
