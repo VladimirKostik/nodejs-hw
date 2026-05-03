88c74bf31ac381a5447ae85f3608f7d9af6fff31
Vladimir_Kostik <vladimirkostik48@gmail.com>
2026-05-03 10:26:23 +0300

    final fixes

--- src/server.js
+++ src/server.js
@@ -12,7 +12,22 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(pino());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  })
+);
 
 
 
