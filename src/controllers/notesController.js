2ca29d6d3b0d3db302cdc7f52f10d23bea15370c
Vladimir_Kostik <vladimirkostik48@gmail.com>
2026-05-10 23:53:15 +0300

    implement get note by id

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,7 +1,20 @@
+import createHttpError from 'http-errors';
 import { NoteCollection } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
   const notes = await NoteCollection.find();
 
   res.status(200).json(notes);
+};
+
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await NoteCollection.findById(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
 };
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
-import { getAllNotes } from '../controllers/notesController.js';
+import {
+  getAllNotes,
+  getNoteById,
+} from '../controllers/notesController.js';
 
 const router = Router();
 
 router.get('/notes', getAllNotes);
-
+router.get('/notes/:noteId', getNoteById);
 export default router;
