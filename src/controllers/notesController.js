d262fd4c600b24dcde5f18d750d92dbf2981c403
Vladimir_Kostik <vladimirkostik48@gmail.com>
2026-05-11 00:31:08 +0300

    implement update note

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -18,3 +18,39 @@ export const getNoteById = async (req, res) => {
 
   res.status(200).json(note);
 };
+
+export const createNote = async (req, res) => {
+  const note = await NoteCollection.create(req.body);
+
+  res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await NoteCollection.findByIdAndDelete(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const updatedNote = await NoteCollection.findByIdAndUpdate(
+    noteId,
+    req.body,
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedNote) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(updatedNote);
+};
